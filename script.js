6a8f3f58e28d018c32c3eeb14a3e56e65269344d
mustran <ademimusli000@gmail.com>
2019-07-21 13:58:32 +0200

    solution added

---
+++ script.js
@@ -0,0 +1,17 @@
+function palindrome(str){
+    //replace every non character with a space and make the string lowercase
+    str = str.replace(/[^a-z0-9+]+/gi, '').toLowerCase();
+    if(str === reverseString(str))
+    return true;
+    return false;
+}
+
+function reverseString(string){
+    let splitWord = string.split('');
+    splitWord.reverse();
+    string = splitWord.join('');
+    return string
+}
+
+
+console.log(palindrome("not a palindrome"));
